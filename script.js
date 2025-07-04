6d96ead7ab3015d920c87ea67ec7ed9a1f3597a5
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-07-04 13:32:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let saveInput = document.querySelector('input[type="submit"]')
+saveInput.addEventListener("click",()=>{
+	let fontSize = document.querySelector('#fontsize').value
+	let fontColor = document.querySelector('input[type="color"]').value
+	// console.log(fontSize,fontColor)
+	document.cookie = "fontcolor="+fontColor+"; expires= Fri, 25 June 2023 12:00:00 UTC; path=/";
+	document.cookie = "fontsize="+fontSize+"; expires= Fri, 25 June 2023 12:00:00 UTC; path=/";
+}) 
+
+function showCookieValue(){
+let fontSizeCookie = getCookie("fontsize")
+	if(fontSizeCookie){
+		document.querySelector('#fontsize').value = fontSizeCookie 
+	}
+	let fontColorCookie =getCookie("fontcolor")
+	if(fontColorCookie){
+		document.querySelector('input[type="color"]').value = fontColorCookie 
+	}
+
+	
+}
+
+function getCookie(key){
+	let cookies =  document.cookie.split("; ")
+	 .find((row) => row.startsWith(key))
+	if(cookies){
+		return cookies.split("=")[1]
+	}else{
+		return undefined
+	}
+	
+}
